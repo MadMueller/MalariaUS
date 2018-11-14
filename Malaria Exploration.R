f3fa532f4cfbdb7826b992fa53bef19d0a155b7d
Madeleine <43894984+MadMueller@users.noreply.github.com>
2018-11-14 15:06:18 -0600

    Updated exploration - looking at outliers

--- Malaria Exploration.R
+++ Malaria Exploration.R
@@ -18,9 +18,9 @@ locs2<-unique(d[,c("Admin1Name","Admin1ISO")])
 locs2
 unique(d$Subpopulation) #unsure about this - Civilian, Non specified, or Military 
 unique(d$PartOfCumulativeCountSeries) #0 or 1 - is the cumulative redundant or does it show data for different dates?
-summary(d$CountValue) #a max of 735??? How can I find out which state/time this is associated with?
-#also how can summary be split to only show cumulative data or only noncumulative
+summary(d$CountValue) #a max of 735??? 
+#also how can summary() be split to only show cumulative data or only noncumulative
 d<-d[,-c(1,2,3,4,5,6,7,8,15,17,18,19)]
 names(d)
-
+d[which.max(d$CountValue),]#Makes sense that this is part of the cumulative series but this still seem like an outlier?
 
